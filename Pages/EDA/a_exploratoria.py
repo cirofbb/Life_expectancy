625aaec1669bdc8f1dea396e878c061d5ea11f73
cirofbb <154011878+cirofbb@users.noreply.github.com>
2023-12-19 16:58:18 -0300

    Update a_exploratoria.py

--- Pages/EDA/a_exploratoria.py
+++ Pages/EDA/a_exploratoria.py
@@ -9,7 +9,7 @@ def a_exploratoria():
     # Carregando a base de dados
     try:
         df = pd.read_csv(
-            'https://raw.githubusercontent.com/claudiopickersgill/repoteste/main/data/Life-Expectancy-Data-Updated.csv')
+            'https://raw.githubusercontent.com/cirofbb/Life_expectancy/main/data/Life-Expectancy-Data-Updated.csv')
     except Exception as e:
         print(f"Erro ao ler o arquivo CSV: {e}")
 
