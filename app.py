88f84968a4a6605f3f3d0a2c1fd3ed326fd48f93
cirofbb <154011878+cirofbb@users.noreply.github.com>
2023-12-19 16:51:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from Pages.principal import principal
-from Pages.a_exploratoria import a_exploratoria
-from Pages.a_qual_dados import a_qual_dados
-from Pages.resultados import resultados
+from Pages.EDA import a_exploratoria
+from Pages.quali import a_qual_dados
+from Pages.results import resultados
 
 st.set_page_config(layout="wide")
 
@@ -14,8 +14,8 @@ menu = st.sidebar.selectbox('Selecione uma Página', [
 if menu == 'Principal':
     principal.principal()
 elif menu == 'Análise exploratória':
-    a_exploratoria.a_exploratoria()
+    EDA.a_exploratoria()
 elif menu == 'Análise de qualidade de dados':
-    a_qual_dados.a_qual_dados()
+    quali.a_qual_dados()
 elif menu == 'Resultado do modelo de regressão linear':
-    resultados.resultados()
+    results.resultados()
