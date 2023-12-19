5348bf35bbfaf6f8fefa969d1fa18d0efd9f56d9
cirofbb <154011878+cirofbb@users.noreply.github.com>
2023-12-19 20:20:55 -0300

    Update principal.py

--- Pages/principal/principal.py
+++ Pages/principal/principal.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 def principal():
-    st.title('Dashboard de Análise de Expectativa de Vida')
+    st.title('Dashboard - Análise de Expectativa de Vida')
     st.write(
         'Navegue a partir do menu lateral para exibir as diferentes fases do projeto')
